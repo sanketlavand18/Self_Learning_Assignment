b8f65d5b938ba68c388a893986b01f1c273af4a2
Sanket Lavand 18 <sanketlavand1824@gmail.com>
2026-03-14 15:51:06 +0530

    Add files via upload

---
+++ WPT/linkchange.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $("#changeBtn").click(function(){
+
+        $("#myLink").attr("href","https://www.wikipedia.org");
+        $("#myLink").text("Go to Wikipedia");
+
+    });
+
+});
